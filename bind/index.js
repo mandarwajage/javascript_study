a753a0c58a954b1b2751fcc3e988543264d36fcd
mandar wajage <mandarwajage@gmail.com>
2023-10-15 00:44:22 +0530

    Polyfill for bind

--- bind/index.js
+++ bind/index.js
@@ -8,17 +8,28 @@ let name2 = {
   lastname: "Kohli"
 } 
 
-let printName = function () {
-    console.log(this.firstname + " " + this.lastname); 
+let printName = function (hometown, state) {
+    console.log(this.firstname + " " + this.lastname + " " + hometown+ " " + state); 
 }
 
-let printMyName1 = printName.bind(name1); //sachin Tendulkar
-let printMyName2 = printName.bind(name2); //virat Kohli
+let printMyName1 = printName.bind(name1,"mumbai"); //sachin Tendulkar
+let printMyName2 = printName.bind(name2, "delhi"); //virat Kohli
 
-printMyName1(); 
-printMyName2(); 
+printMyName1("maharashtra"); 
+printMyName2("Delhi_state"); 
 
-////////////
+//////////// 
 
+Function.prototype.Mybind = function(...args) { 
+ // this --> printName()
+  let obj = this;
+  params = args.slice(1); // extracting 1st element ; object
+  return function(...args2) //printMyName3("New_State")
+  {
+    obj.apply(args[0], [...params, ...args2]);
+  }
+}
 
+let printMyName3 = printName.Mybind(name1 , "pune"); //sachin Tendulkar
+printMyName3("New_State");
 
